838c99e6a5bdc3e352043020c4112df1ff3e3581
rcucho <38509900+rcucho@users.noreply.github.com>
2021-05-21 11:18:26 -0500

    Add files via upload

---
+++ mstech_jasatec2/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Jasatec - 2da entrega',
+    'version': '14.0.1.0.0',
+    'author': 'MSTECH',
+    'category' : 'Technical Configuration',
+    'website': 'http://www.mstech.pe',
+    'depends': [
+        'mrp',
+    ],
+    'data': [
+        'views/report_mrp_jasatec.xml',
+        'report/report_mrp_service.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'sequence': 1,
+}
