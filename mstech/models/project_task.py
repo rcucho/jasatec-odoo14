c7c2fb7d5ec8c000aa9660552f304bbe660006cd
rcucho <38509900+rcucho@users.noreply.github.com>
2021-05-26 11:04:51 -0500

    Update project_task.py

--- mstech/models/project_task.py
+++ mstech/models/project_task.py
@@ -4,6 +4,7 @@ from odoo.exceptions import UserError
 class FormulariosColumnaConectada(models.Model):
     _inherit = "project.task"
     
-    hola_g = fields.Char(string="Hola")
     partner_type_vat = fields.Char(related='partner_id.l10n_latam_identification_type_id.name', readonly=True)
     partner_number_vat = fields.Char(related='partner_id.vat', readonly=True)
+    sale_order_date = fields.Char(related='sale_order_id.date_order', readonly=True)
+    
