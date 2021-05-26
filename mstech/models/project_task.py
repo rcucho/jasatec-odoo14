6a665e52ec8e8cf643cc92ddce32a210ed70e88d
rcucho <38509900+rcucho@users.noreply.github.com>
2021-05-26 16:31:28 -0500

    Update project_task.py

--- mstech/models/project_task.py
+++ mstech/models/project_task.py
@@ -9,4 +9,4 @@ class FormulariosColumnaConectada(models.Model):
     partner_number_vat = fields.Char(related='partner_id.vat', readonly=True)
     sale_order_date = fields.Datetime(related='sale_order_id.date_order', readonly=True, string="Fecha Orden de Venta")
     parent_res_contact=fields.Char(related='partner_id.child_ids.name', readonly=True, string="Contacto relacionado")
-   
+    parent_contact_function=fields.Char(related='partner_id.child_ids.function', readonly=True, striing="Puesto contacto relacionado")   
