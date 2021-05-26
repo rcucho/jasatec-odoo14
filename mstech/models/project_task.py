5e5505d452d8a8f9c8539d42f2f41f709fda1484
rcucho <38509900+rcucho@users.noreply.github.com>
2021-05-26 11:35:35 -0500

    Update project_task.py

--- mstech/models/project_task.py
+++ mstech/models/project_task.py
@@ -1,4 +1,5 @@
 from odoo import models,fields,api,_
+from datetime import timedelta, datetime
 from odoo.exceptions import UserError
 
 class FormulariosColumnaConectada(models.Model):
@@ -6,5 +7,5 @@ class FormulariosColumnaConectada(models.Model):
     
     partner_type_vat = fields.Char(related='partner_id.l10n_latam_identification_type_id.name', readonly=True)
     partner_number_vat = fields.Char(related='partner_id.vat', readonly=True)
-    sale_order_date = fields.Char(related='sale_order_id.date_order', readonly=True)
+    sale_order_date = fields.Datetime(related='sale_order_id.date_order', readonly=True)
     
