bd496f7c4546b0944fe4ed5c1dbac1ffd00db80f
rcucho <38509900+rcucho@users.noreply.github.com>
2021-05-25 23:47:01 -0500

    Add files via upload

---
+++ mstech/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Jasatec - Mstech',
+    'version': '14.0.1.0.0',
+    'author': 'MSTECH',
+    'category' : 'Technical Configuration',
+    'website': 'http://www.mstech.pe',
+    'depends': [
+        'industry_fsm',
+    ],
+    'data': [
+        'views/view_project_task.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'sequence': 1,
+}
