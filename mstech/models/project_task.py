8acb8b4fef39f4fe5972b2f38ca22bb3ec6a11f1
rcucho <38509900+rcucho@users.noreply.github.com>
2021-05-26 10:33:17 -0500

    Update project_task.py

--- mstech/models/project_task.py
+++ mstech/models/project_task.py
@@ -5,3 +5,5 @@ class FormulariosColumnaConectada(models.Model):
     _inherit = "project.task"
     
     hola_g = fields.Char(string="Hola")
+    partner_type_vat = fields.Char(related='partner_id.l10n_latam_identification_type_id.name', readonly=True)
+    partner_number_vat = fields.Char(related='partner_id.vat', readonly=True)
