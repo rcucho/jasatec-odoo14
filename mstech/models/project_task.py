58d5fba624619389366905afd4a40bb31ade0898
rcucho <38509900+rcucho@users.noreply.github.com>
2021-06-08 18:54:51 -0500

    Update project_task.py

--- mstech/models/project_task.py
+++ mstech/models/project_task.py
@@ -44,7 +44,7 @@ class FormulariosColumnaConectada(models.Model):
     sale_line_product = fields.Many2one('sale.order.line',string='Orden de linea')
     sale_line_product2 = fields.One2many('sale.order.line','task_id',string='Orden de linea 2')
     
-    sale_lien_product3 = fields.One2many('sale.order', 'order_line', string="orden de linea")
+    sale_line_product3 = fields.One2many('sale.order', 'order_line', string="orden de linea")
     
         
 class PointofSale(models.Model):
