246d01bde6350a88c2e67bcfc8c1c89241e083cb
rcucho <38509900+rcucho@users.noreply.github.com>
2021-06-11 08:16:54 -0500

    Update project_task.py

--- mstech/models/project_task.py
+++ mstech/models/project_task.py
@@ -36,7 +36,7 @@ class FormulariosColumnaConectada(models.Model):
     
     sale_line_product4 = fields.One2many(related='sale_line_id.order_id.order_line',string='Orden de linea aaaa')
     #---------------------------------------------------------------------------------------------
-    cliente_task = fields.Many2one('res.partner', string="Cliente de Tarea")
+    cliente_task = fields.Many2one('res.partner', string="Cliente de Tarea", compute="_onchange_cliente_task")
     sale_line_product = fields.Many2many(comodel_name='sale.order.line', relation='relation_task_product', column1='project_task_id', column2='sale_order_line_id', string ='Productos vendidos', compute='_compute_sale_line_product')
     fecha_inicio = fields.Date(string='Fecha de inicio de tarea', compute='_onchange_fecha_inicio')
     fecha_fin = fields.Date(string="Fecha fin de Tarea", compute='_onchange_fecha_fin')
