4bcac0bbeac27df2f97863cd7f917ce66fe87db4
rcucho <38509900+rcucho@users.noreply.github.com>
2021-06-11 09:17:36 -0500

    Update project_task.py

--- mstech/models/project_task.py
+++ mstech/models/project_task.py
@@ -67,7 +67,7 @@ class FormulariosColumnaConectada(models.Model):
     @api.onchange('planned_date_begin')
     def _onchange_fecha_inicio(self):
         for record in self:
-            if record.planned_date_begin:
+            #if record.planned_date_begin:
                 record.fecha_inicio = record.planned_date_begin.strftime("%Y-%m-%d")
         
     @api.onchange('planned_date_end')
