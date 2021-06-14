5b0dcc3ca0f5a28eeed2f56b28f2efc6e59e2e01
rcucho <38509900+rcucho@users.noreply.github.com>
2021-06-14 10:23:16 -0500

    Update project_task.py

--- mstech/models/project_task.py
+++ mstech/models/project_task.py
@@ -91,7 +91,7 @@ class FormulariosColumnaConectada(models.Model):
             record.mov_herramienta.do_unreserve()
         return True
     
-    def action_confirm_picking():
+    def action_confirm_picking(self):
         for record in self:
             record.task_picking.action_confirm()
         return True
