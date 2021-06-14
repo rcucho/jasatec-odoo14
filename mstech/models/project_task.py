3102d8075aea438bfe82b4526d02383d966c090c
rcucho <38509900+rcucho@users.noreply.github.com>
2021-06-14 08:01:14 -0500

    Update project_task.py

--- mstech/models/project_task.py
+++ mstech/models/project_task.py
@@ -113,12 +113,12 @@ class StockPickingTask(models.Model):
     
     @api.model
     def create(self, vals):
-        defaults = self.default_get(['name', 'picking_type_id'])
-        picking_type = self.env['stock.picking.type'].browse(vals.get('picking_type_id', defaults.get('picking_type_id')))      
-        if self.picking_task:         
-            if vals.get('name', '/') == '/' and defaults.get('name', '/') == '/' and vals.get('picking_type_id', defaults.get('picking_type_id')):
-                vals['name'] = picking_type.sequence_id.next_by_id()
-                    
-        res = super(self).create(vals)
+        #defaults = self.default_get(['name', 'picking_type_id'])
+        #picking_type = self.env['stock.picking.type'].browse(vals.get('picking_type_id', defaults.get('picking_type_id')))      
+        if self.picking_task:
+            res = super(self).create(vals)
+            #if vals.get('name', '/') == '/' and defaults.get('name', '/') == '/' and vals.get('picking_type_id', defaults.get('picking_type_id')):
+                #vals['name'] = picking_type.sequence_id.next_by_id()
+
         return res
     
