90b99f483a1e7304bfa87dfb79a410b66e09c574
rcucho <38509900+rcucho@users.noreply.github.com>
2021-06-21 11:41:37 -0500

    Add files via upload

---
+++ mstech/models/account_move.py
@@ -0,0 +1,10 @@
+from odoo import models,fields,api,_
+from datetime import timedelta, datetime
+from odoo.exceptions import UserError
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+    
+    jasatec_os= fields.Char(string='O/S')
+    jasatec_constancia= fields.Char(string='Constancia')
+    
